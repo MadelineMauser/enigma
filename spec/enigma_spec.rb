672cc2799c04633b31322e72f33748946d391ffd
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-07 14:45:17 -0600

    Add test for #decrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -19,4 +19,9 @@ describe '#shifts' do
     expect(@enigma.shifts('02715', '040895')).to eq([3, 27, 73, 20])
   end
 end
+describe '#decrypt' do
+  it 'can decrypt a given string with a given key and date' do
+    expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq({decryption: 'hello world', key: '02715', date: '040895'})
+  end
+end
 end
