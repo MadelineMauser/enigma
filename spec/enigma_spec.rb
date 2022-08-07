3891c277c9416a7b75cf92bfbe972a2760a78e93
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-07 12:29:33 -0600

    Add test for #encrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,9 @@ describe 'Enigma instantiation' do
     expect(@enigma).to be_an_instance_of(Enigma)
   end
 end
+describe '#encrypt' do
+  it 'can encrypt a given string with a given key and date' do
+    expect(@enigma.encrypt('Hello world', '040895', '040895')).to eq({encryption: 'keder ohulw', key: '040895', date: '040895'})
+  end
+end
 end
