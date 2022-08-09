70f82984377fcf2d2e885cb4a88a194c5a8ccf9a
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 15:55:04 -0600

    Finish decrypt runner file

--- decrypt.rb
+++ decrypt.rb
@@ -1,3 +1,10 @@
 require './lib/enigma'
 
-enigma - Enigma.new
+enigma = Enigma.new
+
+decryption = enigma.decrypt(File.read(ARGV[0]).chomp, ARGV[2], ARGV[3])
+
+new_file = File.open(ARGV[1], 'w')
+new_file.write(decryption[:decryption])
+
+puts "Created '#{ARGV[1]}' with the key #{decryption[:key]} and date #{decryption[:date]}"
