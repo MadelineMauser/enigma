37b1e5e7ebbe1bda37d7b463a83e5a58ceef948d
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 16:59:40 -0600

    Change Encryption to not be a child of Engima

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,5 +1,18 @@
-require 'date'
-require './lib/enigma'
-class Encryption < Enigma
+class Encryption
 
+  def encrypt(message, key = random_key, date = Date.today.strftime('%d%m%y'))
+    encryption = ''
+    working_index = 0
+    message.downcase.split('').each do |character|
+      if @characer_set.any?(character)
+        current_shift = Shiftable.shifts(key, date)[working_index % 4]
+        char_index = @characer_set.find_index(character)
+        encryption.concat(@characer_set[(char_index + current_shift) % 27])
+        working_index += 1
+      else
+        encryption.concat(character)
+      end
+    end
+    {encryption: encryption, key: key, date: date}
+  end
 end
