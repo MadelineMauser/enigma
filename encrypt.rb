f00ae2fc6933a0f7104153a696c30ab321dd32e4
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 15:51:06 -0600

    Finish encrypt runner file

--- encrypt.rb
+++ encrypt.rb
@@ -3,3 +3,8 @@ require './lib/enigma'
 enigma = Enigma.new
 
 encryption = enigma.encrypt(File.read(ARGV[0]).chomp)
+
+new_file = File.open(ARGV[1], 'w')
+new_file.write(encryption[:encryption])
+
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
