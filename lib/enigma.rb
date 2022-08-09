377403d1f4d4508de4d3ea9485d8ec7c51abcfa9
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 17:12:31 -0600

    Move #decrypt into Decryption class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,19 +13,7 @@ class Enigma
   end
 
   def decrypt(message, key, date = Date.today.strftime('%d%m%y'))
-    decryption = ''
-    working_index = 0
-    message.downcase.split('').each do |character|
-      if @character_set.any?(character)
-        current_shift = Shiftable.shifts(key, date)[working_index % 4]
-        char_index = @character_set.find_index(character)
-        decryption.concat(@character_set[(char_index - current_shift) % 27])
-        working_index += 1
-      else
-        decryption.concat(character)
-      end
-    end
-    {decryption: decryption, key: key, date: date}
+    
   end
 
   def random_key
