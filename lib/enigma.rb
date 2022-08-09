b8eea0487e1996490b4b0768ffc39f4769533320
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 15:28:13 -0600

    Fix #encrypt to handle special characters

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,11 +9,14 @@ class Enigma
     encryption = ''
     working_index = 0
     message.downcase.split('').each do |character|
+      if @characer_set.any?(character)
       current_shift = shifts(key, date)[working_index % 4]
       char_index = @characer_set.find_index(character)
-      binding.pry
       encryption.concat(@characer_set[(char_index + current_shift) % 27])
       working_index += 1
+      else
+        encryption.concat(character)
+      end
     end
     {encryption: encryption, key: key, date: date}
   end
