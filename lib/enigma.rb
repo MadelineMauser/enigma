ebec9ef1941321539f117f89337c44d60ed20fc1
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-09 16:09:14 -0600

    Refactor #shifts to be shorter

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -38,22 +38,11 @@ class Enigma
   end
 
   def shifts(key, date)
-    a_key = key[0, 2]
-    b_key = key[1, 2]
-    c_key = key[2, 2]
-    d_key = key[3, 2]
-
     date_offset = ((date.to_i) * (date.to_i)).to_s[-4..-1]
-    a_offset = date_offset[0]
-    b_offset = date_offset[1]
-    c_offset = date_offset[2]
-    d_offset = date_offset[3]
-
-    a_shift = a_key.to_i + a_offset.to_i
-    b_shift = b_key.to_i + b_offset.to_i
-    c_shift = c_key.to_i + c_offset.to_i
-    d_shift = d_key.to_i + d_offset.to_i
-
+    a_shift = key[0, 2].to_i + date_offset[0].to_i
+    b_shift = key[1, 2].to_i + date_offset[1].to_i
+    c_shift = key[2, 2].to_i + date_offset[2].to_i
+    d_shift = key[3, 2].to_i + date_offset[3].to_i
     [a_shift, b_shift, c_shift, d_shift]
   end
 
